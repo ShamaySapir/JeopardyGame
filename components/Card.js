6c4a59d8b43192bc7f3880fbdc1e5aa72438a3f2
ShamaySapir <shamay.sapir@gmail.com>
2022-06-24 05:05:31 +0300

    wip

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,80 @@
-// import * as React from "react";
+import * as React from "react";
 import styles from "../styles/Home.module.css";
-import FullScreenDialog from "./Dialog";
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  Button,
+  Dialog,
+  ListItemText,
+  ListItem,
+  List,
+  Divider,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  // Grid,
+  Slide,
+  // Link,
+} from "@mui/material";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Card = (props) => {
-  const { prize, question, answer, ...other } = props;
+  const { prize, question, answer } = props;
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <div className={styles.card}>
-      <h2>{prize} &rarr;</h2>
-      <p>Find in-depth information about Next.js features and API.</p>
-      <FullScreenDialog question={question} answer={answer} />
-    </div>
+    <>
+      <div className={styles.card} onClick={handleClickOpen}>
+        <h2>{prize}</h2>
+      </div>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              {question}
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              {answer}
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <List>
+          <ListItem button>
+            <ListItemText primary="Phone ringtone" secondary="Titania" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText
+              primary="Default notification ringtone"
+              secondary="Tethys"
+            />
+          </ListItem>
+        </List>
+      </Dialog>
+    </>
   );
 };
 
