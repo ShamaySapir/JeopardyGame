f4d68f9437e80fbd973a4ab4a7775852a085e7e7
Ran Shamay <ranshamay@microsoft.com>
2022-07-07 13:59:27 +0300

    wip

--- components/modal.js
+++ components/modal.js
@@ -2,25 +2,28 @@ import * as React from "react";
 import { Modal, Box, Typography, Grid } from "@mui/material";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
-
+import { sample } from "lodash";
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  // width: 100,
   bgcolor: "background.paper",
-  // border: "2px solid #000",
   boxShadow: 24,
   pt: 2,
   px: 4,
   pb: 3,
 };
 
-const failedTexts = ["פאדיחות, לא נורא פעם הבאה ילך יותר טוב"];
+const failedTexts = [
+  "פאדיחות, לא נורא פעם הבאה ילך יותר טוב",
+  "פספסת אהו אהו פספסת",
+  "שווה לשקול שבת אצל חיים להשלים פערים",
+];
+const successTexts = ["מצוין!", "ניכר שיש היכרות טובה! סחתיין", "בינגו!"];
 const generateModalContent = (isSuccess) => {
   const Comp = isSuccess ? ThumbUpIcon : ThumbDownIcon;
-  const text = isSuccess ? "מצוין!" : "פאדיחות, לא נורא פעם הבאה ילך יותר טוב";
+  const text = isSuccess ? sample(successTexts) : sample(failedTexts);
   return (
     <Grid container direction="column">
       <Grid container item justifyContent="center">
